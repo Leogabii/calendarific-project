541a9f1593e01ae637a077b6682ed403ac7a6f1d
Leogabii <122978943+Leogabii@users.noreply.github.com>
2024-12-06 19:55:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import CalendarController from './controllers/CalendarController.js';
+//import CalendarController from './controllers/CalendarController.js';
+import CalendarController from '../controllers/CalendarController.js';
 
 // Cargar variables de entorno
 dotenv.config();
